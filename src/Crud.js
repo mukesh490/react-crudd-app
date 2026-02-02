307e233febb26fa076c1c74e8ab6359a85c3b96b
Mukesh Yadav <mukeshyadavbca777@gmail.com>
2026-02-02 13:45:23 +0530

    table

--- src/Crud.js
+++ src/Crud.js
@@ -1,6 +1,14 @@
-import React, {  useState } from 'react'
-import { Formik, Form, Field } from 'formik'
+  import React, { useState } from 'react'
+import { Formik, Form, Field, ErrorMessage } from 'formik'
+import * as Yup from "yup";
 
+const UserSchema = Yup.object().shape({
+  name: Yup.string().required("Name is required"),
+  email: Yup.string().required("Email is required"),
+  phone: Yup.string().required("Phone is required")
+  .matches(/^[0-9]{10}$/, "Phone must be 10 digits"),
+  
+});
 export default function Crud() {
     const [formData, setFormData] = useState([]);
     const [editUser, setEditUser] = useState(null)
@@ -51,7 +59,7 @@ export default function Crud() {
     return (
         <div className=' min-h-screen bg-gray-100 flex  justify-center item-start py-10'>
             <div className='w-full max-w-3xl bg-white shadow-lg rounded-lg p-6'>
-                <h1 className='text-center text-2xl font-semibold mb-6'>Add user</h1>
+                <h1 className='text-center text-2xl font-semibold mb-6'>{`${editUser?'Update User':'Add User'}`}</h1>
                 <Formik
                     enableReinitialize
                     initialValues={{
@@ -59,7 +67,7 @@ export default function Crud() {
                         email: editUser?.email || '',
                         phone: editUser?.phone || ''
                     }}
-                    // validationSchema={PostSchema},
+                    validationSchema={UserSchema}
                     onSubmit={(values, { resetForm }) =>
                         handleSubmit(values, resetForm)
                     }
@@ -69,18 +77,33 @@ export default function Crud() {
                             <div>
                                 <label className="block text-sm font-medium text-gray-700 mb-1">Name</label>
                                 <Field type='text' name='name' placeholder="Enter name" className='w-full rounded-md border border-gray-300 py-2 px-3 focus:outline-none focus:ring-2 focus:ring-blue-500 ' />
+                                <ErrorMessage
+                                    name="name"
+                                    component="p"
+                                    className="text-red-500 text-sm mt-1"
+                                />
                             </div>
                             <div>
                                 <label className="block text-sm font-medium text-gray-700 mb-1">
                                     Email
                                 </label>
                                 <Field type='email' name='email' placeholder="Enter email" className='w-full border rounded-md border-gray-300 py-2 px-3 focus:outline-none focus:ring-2 focus:ring-blue-500' />
+                                <ErrorMessage
+                                    name="email"
+                                    component="p"
+                                    className="text-red-500 text-sm mt-1"
+                                />
                             </div>
                             <div>
                                 <label className="block text-sm font-medium text-gray-700 mb-1">
                                     Phone
                                 </label>
-                                <Field type='number' name='phone' placeholder="Enter phone No" className='w-full border rounded-md border-gray-300 py-2 px-3 focus:outline-none focus:ring-2 focus:ring-blue-500' />
+                                <Field type='text' name='phone' placeholder="Enter phone No" className='w-full border rounded-md border-gray-300 py-2 px-3 focus:outline-none focus:ring-2 focus:ring-blue-500' />
+                                <ErrorMessage
+                                    name="phone"
+                                    component="p"
+                                    className="text-red-500 text-sm mt-1"
+                                />
                             </div>
 
 
